f34566deb10a49110d34eecff3baaab6b09b8776
RuiTingM <302854619@>
2017-08-21 13:42:52 +0800

    增加登陆界面

---
+++ GitDemo2/app/src/main/java/com/example/rtm/gitdemo/LoginAty.java
@@ -0,0 +1,17 @@
+package com.example.rtm.gitdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by RTM on 2017/8/21.
+ */
+
+public class LoginAty extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
