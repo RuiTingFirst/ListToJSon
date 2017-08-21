d2fbc7c344a79c9cb8e189fdb3d8a5d287f2de0f
RuiTingM <302854619@>
2017-08-21 14:59:00 +0800

    第一次提交
    # Conflicts:
    #	GitDemo2/app/src/main/java/com/example/rtm/gitdemo/MainActivity.java
    
    Signed-off-by: RuiTingM <302854619@>
    
    # Conflicts:
    #	GitDemo2/app/src/main/java/com/example/rtm/gitdemo/MainActivity.java

--- GitDemo2/app/src/main/java/com/example/rtm/gitdemo/MainActivity.java
+++ GitDemo2/app/src/main/java/com/example/rtm/gitdemo/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 练习使用git
-        
+
     }
 
     @Override
